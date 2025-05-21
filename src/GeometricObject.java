e3c9150135444fc286ec1fb809062c4e3ffc90f9
DmitryGorodenskiy <Vlad.Gorod.69@yandex.ru>
2025-05-21 15:48:27 +0300

    LAB8

--- src/GeometricObject.java
+++ src/GeometricObject.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import java.util.Date;
 
 public class GeometricObject {
+    private String color = "white";
+    private boolean filled = false;
+    private Date dateCreated;
+
+    public GeometricObject() {
+        dateCreated = new Date();
+    }
+
+    public GeometricObject(String color, boolean filled) {
+        this();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    @Override
+    public String toString() {
+        return "GeometricObject created on " + dateCreated +
+                "\nColor: " + color + " and filled: " + filled;
+    }
 }
